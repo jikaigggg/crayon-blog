d0d61eecf4d8a1e55203ef99e2e93f9bb6954f7a
Crayonkai <jikaigg@gmail.com>
2023-09-22 10:31:57 +0800

    项目初始化

---
+++ crayon-blog-web/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>HOME</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'crayonHome'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ crayon-blog/src/main/java/com/jikaigg/blog/CrayonBlogApplication.java
@@ -0,0 +1,11 @@
+package com.jikaigg.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrayonBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrayonBlogApplication.class, args);
+    }
+}
